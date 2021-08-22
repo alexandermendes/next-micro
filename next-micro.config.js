c634ee87ff6bce78b00279d0b16196c3ee1ce418
alexmendes <alexander.mendes@immediate.co.uk>
2021-08-22 17:35:40 +0100

    refactor: tweak next-micro name

---
+++ next-micro.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  port: 3000,
+  autostart: true,
+  services: [
+    {
+      rootDir: path.join(__dirname, 'example', 'frontend'),
+    },
+    {
+      port: 3001,
+      script: 'start.js',
+      rootDir: path.join(__dirname, 'example', 'api'),
+      routes: ['/api/.*'],
+      ttl: 60000,
+      env: {
+        PORT: 3001,
+      },
+    },
+  ],
+}
